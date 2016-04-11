2c791cf224d388f90a0f6bb4f764c8a18284c0dc
Ivan Bodrov <ibodrov@gmail.com>
2016-04-11 23:19:11 +0300

    glfw init

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
+extern crate cgmath;
 #[macro_use]
 extern crate gfx;
-extern crate gfx_window_glutin;
-extern crate gfx_device_gl;
-extern crate glutin;
-extern crate cgmath;
-extern crate time;
+extern crate gfx_app;
 
-use gfx::traits::FactoryExt;
-use gfx::Device;
-
-pub type ColorFormat = gfx::format::Rgba8;
-pub type DepthFormat = gfx::format::DepthStencil;
+pub use gfx_app::{ColorFormat, DepthFormat};
 
 gfx_vertex_struct!(Vertex {
     pos: [i32; 2] = "a_Pos",
@@ -25,7 +18,7 @@ gfx_pipeline!(pipe {
     vbuf: gfx::VertexBuffer<Vertex> = (),
     instance: gfx::InstanceBuffer<Instance> = (),
     transform: gfx::Global<[[f32; 4]; 4]> = "u_Transform",
-    out: gfx::RenderTarget<ColorFormat> = "Target0",
+    out_color: gfx::RenderTarget<ColorFormat> = "Target0",
 });
 
 const TILE_SIZE: i32 = 16;
@@ -41,78 +34,72 @@ const QUAD_INDEX: &'static [u16] = &[0, 1, 2, 1, 3, 2];
 
 const CLEAR_COLOR: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-pub fn main() {
-    use gfx::traits::Factory;
+struct App<R: gfx::Resources>{
+    bundle: pipe::Bundle<R>,
+}
 
-    let builder = glutin::WindowBuilder::new()
-        .with_title("Triangle example".to_string())
-        .with_dimensions(1024, 768);
+impl<R: gfx::Resources> gfx_app::Application<R> for App<R> {
+    fn new<F: gfx::Factory<R>>(mut factory: F, init: gfx_app::Init<R>) -> Self {
+        use gfx::traits::FactoryExt;
 
-    let (window, mut device, mut factory, main_color, _main_depth) =
-        gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
-    let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
-    let pso = factory.create_pipeline_simple(
-        include_bytes!("shader/triangle_150.glslv"),
-        include_bytes!("shader/triangle_150.glslf"),
-        gfx::state::CullFace::Nothing,
-        pipe::new()
-    ).unwrap();
+        let vs = gfx_app::shade::Source {
+            glsl_150: include_bytes!("shader/triangle_150.glslv"),
+            .. gfx_app::shade::Source::empty()
+        };
 
-    let instance_cols = 1024 / TILE_SIZE;
-    let instance_rows = 768 / TILE_SIZE;
-    let instance_count = (instance_rows * instance_cols) as u32;
+        let ps = gfx_app::shade::Source {
+            glsl_150: include_bytes!("shader/triangle_150.glslf"),
+            .. gfx_app::shade::Source::empty()
+        };
 
-    let quad_instances = {
-        let mut attributes = (0..instance_count).map(|_| Instance { translate: [0, 0] }).collect::<Vec<_>>();
-        for i in 0..instance_rows {
-            for j in 0..instance_cols {
-                let idx = (i * instance_cols + j) as usize;
-                let t = &mut attributes[idx].translate;
-                t[0] = j * TILE_SIZE;
-                t[1] = i * TILE_SIZE;
-            }
-        } 
-        factory.create_buffer_const(&attributes, gfx::BufferRole::Vertex, gfx::Bind::empty()).unwrap()
-    };
+        let instance_cols = 800 / TILE_SIZE;
+        let instance_rows = 520 / TILE_SIZE;
+        let instance_count = (instance_rows * instance_cols) as u32;
 
-    let (vertex_buffer, mut slice) = factory.create_vertex_buffer_indexed(&QUAD, QUAD_INDEX);
-    slice.instances = Some((instance_count, 0));
-    let proj = cgmath::ortho(0.0, 1024.0, 768.0, 0.0, -1.0, 1.0);
+        let quad_instances = {
+            let mut attributes = (0..instance_count).map(|_| Instance { translate: [0, 0] }).collect::<Vec<_>>();
+            for i in 0..instance_rows {
+                for j in 0..instance_cols {
+                    let idx = (i * instance_cols + j) as usize;
+                    let t = &mut attributes[idx].translate;
+                    t[0] = j * TILE_SIZE;
+                    t[1] = i * TILE_SIZE;
+                }
+            } 
+            factory.create_buffer_const(&attributes, gfx::BufferRole::Vertex, gfx::Bind::empty()).unwrap()
+        };
 
-    let data = pipe::Data {
-        vbuf: vertex_buffer,
-        instance: quad_instances,
-        transform: proj.into(),
-        out: main_color
-    };
+        let (vertex_buffer, mut slice) = factory.create_vertex_buffer_indexed(&QUAD, QUAD_INDEX);
+        slice.instances = Some((instance_count, 0));
 
-    let mut t1 = 0.0;
-    let mut frames = 0;
+        let pso = factory.create_pipeline_simple(
+            vs.select(init.backend).unwrap(),
+            ps.select(init.backend).unwrap(),
+            gfx::state::CullFace::Back,
+            pipe::new()
+        ).unwrap();
 
-    'main: loop {
-        // loop over events
-        for event in window.poll_events() {
-            match event {
-                glutin::Event::KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Escape)) |
-                glutin::Event::Closed => break 'main,
-                _ => {},
-            }
-        }
+        let proj = cgmath::ortho(0.0, 800.0, 520.0, 0.0, -1.0, 1.0);
 
-        // draw a frame
-        encoder.clear(&data.out, CLEAR_COLOR);
-        encoder.draw(&slice, &pso, &data);
-        encoder.flush(&mut device);
-        window.swap_buffers().unwrap();
-        device.cleanup();
+        let data = pipe::Data {
+            vbuf: vertex_buffer,
+            instance: quad_instances,
+            transform: proj.into(),
+            out_color: init.color,
+        };
 
-        frames += 1;
-
-        let t2 = time::precise_time_s();
-        if t2 - t1 > 1.0 {
-            t1 = t2;
-            window.set_title(&format!("FPS: {}", frames));
-            frames = 0;
+        App {
+            bundle: pipe::bundle(slice, pso, data),
         }
     }
+
+    fn render<C: gfx::CommandBuffer<R>>(&mut self, encoder: &mut gfx::Encoder<R, C>) {
+        encoder.clear(&self.bundle.data.out_color, CLEAR_COLOR);
+        self.bundle.encode(encoder);
+    }
+}
+
+pub fn main() {
+    use gfx_app::Application;
+    App::launch_default("Tiles");
 }
