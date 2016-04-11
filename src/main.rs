7743c97451ff403703114fce08e81615a41046ef
Ivan Bodrov <ibodrov@gmail.com>
2016-04-11 22:53:23 +0300

    instancing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate gfx_window_glutin;
 extern crate gfx_device_gl;
 extern crate glutin;
 extern crate cgmath;
+extern crate time;
 
 use gfx::traits::FactoryExt;
 use gfx::Device;
@@ -16,30 +17,37 @@ gfx_vertex_struct!(Vertex {
     color: [f32; 4] = "a_Color",
 });
 
+gfx_vertex_struct!(Instance {
+    translate: [i32; 2] = "a_Translate",
+});
+
 gfx_pipeline!(pipe {
     vbuf: gfx::VertexBuffer<Vertex> = (),
+    instance: gfx::InstanceBuffer<Instance> = (),
     transform: gfx::Global<[[f32; 4]; 4]> = "u_Transform",
     out: gfx::RenderTarget<ColorFormat> = "Target0",
 });
 
-const TILE_SIZE: i32 = 32;
-const TILE_X: i32 = 10 * TILE_SIZE;
-const TILE_Y: i32 = 10 * TILE_SIZE;
+const TILE_SIZE: i32 = 16;
 
 const QUAD: [Vertex; 4] = [
-    Vertex { pos: [ TILE_X + -1 * TILE_SIZE, TILE_Y +  1 * TILE_SIZE ], color: [1.0, 0.0, 0.0, 1.0] },
-    Vertex { pos: [ TILE_X +  1 * TILE_SIZE, TILE_Y +  1 * TILE_SIZE ], color: [0.0, 1.0, 0.0, 1.0] },
-    Vertex { pos: [ TILE_X + -1 * TILE_SIZE, TILE_Y + -1 * TILE_SIZE ], color: [0.0, 0.0, 1.0, 1.0] },
-    Vertex { pos: [ TILE_X +  1 * TILE_SIZE, TILE_Y + -1 * TILE_SIZE ], color: [1.0, 0.0, 0.0, 1.0] },
+    Vertex { pos: [ 0,         TILE_SIZE ], color: [1.0, 0.0, 0.0, 1.0] },
+    Vertex { pos: [ TILE_SIZE, TILE_SIZE ], color: [0.0, 1.0, 0.0, 1.0] },
+    Vertex { pos: [ 0,         0 ], color: [0.0, 0.0, 1.0, 1.0] },
+    Vertex { pos: [ TILE_SIZE, 0 ], color: [0.0, 0.0, 0.0, 1.0] },
 ];
 
-const CLEAR_COLOR: [f32; 4] = [0.1, 0.2, 0.3, 1.0];
+const QUAD_INDEX: &'static [u16] = &[0, 1, 2, 1, 3, 2];
+
+const CLEAR_COLOR: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
 pub fn main() {
+    use gfx::traits::Factory;
+
     let builder = glutin::WindowBuilder::new()
         .with_title("Triangle example".to_string())
-        .with_dimensions(1024, 768)
-        .with_vsync();
+        .with_dimensions(1024, 768);
+
     let (window, mut device, mut factory, main_color, _main_depth) =
         gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
     let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
@@ -50,16 +58,36 @@ pub fn main() {
         pipe::new()
     ).unwrap();
 
-    let quad_index = vec![0, 1, 2, 1, 3, 2u16];
+    let instance_cols = 1024 / TILE_SIZE;
+    let instance_rows = 768 / TILE_SIZE;
+    let instance_count = (instance_rows * instance_cols) as u32;
 
-    let (vertex_buffer, slice) = factory.create_vertex_buffer_indexed(&QUAD, &quad_index[..]);
+    let quad_instances = {
+        let mut attributes = (0..instance_count).map(|_| Instance { translate: [0, 0] }).collect::<Vec<_>>();
+        for i in 0..instance_rows {
+            for j in 0..instance_cols {
+                let idx = (i * instance_cols + j) as usize;
+                let t = &mut attributes[idx].translate;
+                t[0] = j * TILE_SIZE;
+                t[1] = i * TILE_SIZE;
+            }
+        } 
+        factory.create_buffer_const(&attributes, gfx::BufferRole::Vertex, gfx::Bind::empty()).unwrap()
+    };
+
+    let (vertex_buffer, mut slice) = factory.create_vertex_buffer_indexed(&QUAD, QUAD_INDEX);
+    slice.instances = Some((instance_count, 0));
     let proj = cgmath::ortho(0.0, 1024.0, 768.0, 0.0, -1.0, 1.0);
 
     let data = pipe::Data {
         vbuf: vertex_buffer,
+        instance: quad_instances,
         transform: proj.into(),
         out: main_color
     };
+
+    let mut t1 = 0.0;
+    let mut frames = 0;
 
     'main: loop {
         // loop over events
@@ -70,11 +98,21 @@ pub fn main() {
                 _ => {},
             }
         }
+
         // draw a frame
         encoder.clear(&data.out, CLEAR_COLOR);
         encoder.draw(&slice, &pso, &data);
         encoder.flush(&mut device);
         window.swap_buffers().unwrap();
         device.cleanup();
+
+        frames += 1;
+
+        let t2 = time::precise_time_s();
+        if t2 - t1 > 1.0 {
+            t1 = t2;
+            window.set_title(&format!("FPS: {}", frames));
+            frames = 0;
+        }
     }
 }
--- src/shader/triangle_150.glslv
+++ src/shader/triangle_150.glslv
@@ -1,6 +1,7 @@
 #version 150 core
 
 in ivec2 a_Pos;
+in ivec2 a_Translate;
 in vec4 a_Color;
 
 uniform mat4 u_Transform;
@@ -8,6 +9,6 @@ uniform mat4 u_Transform;
 out vec4 v_Color;
 
 void main() {
-    gl_Position = u_Transform * vec4(a_Pos, 0.0, 1.0);
+    gl_Position = u_Transform * vec4(a_Pos + a_Translate, 0.0, 1.0);
     v_Color = a_Color;
 }
